82f19d1835f0f53b53d1d4ed9f602e8a140b2ced
justinmlyle <justin.m.lyle@gmail.com>
2018-05-17 10:05:14 -0500

    Add project files.

---
+++ AspAngularCP1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspAngularCP1.MvcApplication" Language="C#" %>
---
+++ AspAngularCP1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspAngularCP1.Startup))]
+namespace AspAngularCP1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
