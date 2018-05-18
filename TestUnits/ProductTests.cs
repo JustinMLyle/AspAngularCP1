e1d8f42af86938c0bddf3b1bc40103331591d9c4
justinmlyle <justin.m.lyle@gmail.com>
2018-05-17 19:56:14 -0500

    trying to do some sorta testing

---
+++ TestUnits/ProductTests.cs
@@ -0,0 +1,23 @@
+﻿using AspAngularCP1.Models;
+using Xunit;
+
+namespace AspAngularCP1.Test
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void isAnAdmin()
+        {
+
+            // Arrange
+            var p = new Admin { Name = "Test", IsAdmin = true };
+
+            // Act
+            p.IsAdmin = true;
+
+            //Assert
+            Assert.Equal("New Admin Check", p.IsAdmin);
+        }
+
+    }
+}
